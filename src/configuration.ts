9ed578a236f0d57429b89abe02187bd68aa94a86
ZWGKing <nage@163.com>
2021-02-24 18:28:38 +0800

    zwg,V1.0.3;C;1.品牌折扣维护

--- src/configuration.ts
+++ src/configuration.ts
@@ -4,7 +4,7 @@ import { tvs } from "tvs";
 
 export const appConfig: AppConfig = {
     appName: '百灵威系统工程部/pointshopms',
-    version: '1.0.2',
+    version: '1.0.3',
     tvs: tvs,
     loginTop: jnkTop,
     oem: '百灵威',
